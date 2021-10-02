d18d619615ce92eb1b30d275ed57e253c3f4b6eb
Jordi Salvat <jordi@mixmax.com>
2021-10-02 16:29:02 +0200

    fix: unit tests + fix for string literals

---
+++ spec/file.ts
@@ -0,0 +1,6 @@
+/**
+ * This file is needed for the `parserServices` to find, although the
+ * content will be provided by the `code` property of the rule tests.
+ *
+ * DO NOT DELETE.
+ */
--- src/index.ts
+++ src/index.ts
@@ -4,73 +4,73 @@ import {
   TSESLint,
   TSESTree,
 } from '@typescript-eslint/experimental-utils';
-import ts from 'typescript';
+import { TypeFlags } from 'typescript';
 
 const { MemberExpression, Identifier } = AST_NODE_TYPES;
+const { StringLike } = TypeFlags;
 
-export const rules: Record<string, TSESLint.RuleModule<string, unknown[]>> = {
-  'no-trimstart': {
-    meta: {
-      type: 'problem',
-      docs: {
-        description: 'disallow `String.prototype.trimStart`',
-        category: 'Possible Errors',
-        url: 'https://github.com/mixmaxhq/eslint-plugin-mixmax',
-        recommended: false,
-        suggestion: true,
-      },
-      fixable: 'code',
-      schema: [],
-      messages: {
-        prefer: 'Prefer `trimLeft`, as it has wider browser support.',
-      },
+export const noTrimStart: TSESLint.RuleModule<'prefer', unknown[]> = {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'disallow `String.prototype.trimStart`',
+      category: 'Possible Errors',
+      url: 'https://github.com/mixmaxhq/eslint-plugin-mixmax',
+      recommended: false,
+      suggestion: true,
     },
-    create(context) {
-      function check(node: TSESTree.MemberExpression): void {
-        function report() {
-          context.report({
-            messageId: 'prefer',
-            node: node.property,
-            fix: (fixer) => fixer.replaceText(node.property, 'trimLeft'),
-          });
-        }
-
-        if (
-          node.type === MemberExpression &&
-          node.property.type === Identifier &&
-          node.property.name === 'trimStart'
-        ) {
-          // String.prototype.trimStart
-          if (
-            node.object.type === MemberExpression &&
-            node.object.property.type === Identifier &&
-            node.object.property.name === 'prototype' &&
-            node.object.object.type === Identifier &&
-            node.object.object.name === 'String'
-          ) {
-            report();
-            return;
-          }
-
-          // string.trimStart
-          const parserServices = ESLintUtils.getParserServices(context);
-          const checker = parserServices.program.getTypeChecker();
-
-          const originalNode = parserServices.esTreeNodeToTSNodeMap.get(node.object);
-          const type = checker.getTypeAtLocation(originalNode);
-
-          if (type.flags & ts.TypeFlags.String) {
-            report();
-            return;
-          }
-        }
+    fixable: 'code',
+    schema: [],
+    messages: {
+      prefer: 'Prefer `trimLeft`, as it has wider browser support.',
+    },
+  },
+  create(context) {
+    function check(node: TSESTree.MemberExpression): void {
+      function report() {
+        context.report({
+          messageId: 'prefer',
+          node: node.property,
+          fix: (fixer) => fixer.replaceText(node.property, 'trimLeft'),
+        });
       }
 
-      return {
-        MemberExpression: check,
-      };
-    },
+      if (
+        node.type === MemberExpression &&
+        node.property.type === Identifier &&
+        node.property.name === 'trimStart'
+      ) {
+        // String.prototype.trimStart
+        if (
+          node.object.type === MemberExpression &&
+          node.object.property.type === Identifier &&
+          node.object.property.name === 'prototype' &&
+          node.object.object.type === Identifier &&
+          node.object.object.name === 'String'
+        ) {
+          report();
+          return;
+        }
+
+        // string.trimStart
+        const parserServices = ESLintUtils.getParserServices(context);
+        const checker = parserServices.program.getTypeChecker();
+
+        const originalNode = parserServices.esTreeNodeToTSNodeMap.get(node.object);
+        const type = checker.getTypeAtLocation(originalNode);
+        if (type.flags & StringLike) {
+          report();
+          return;
+        }
+      }
+    }
+
+    return {
+      MemberExpression: check,
+    };
   },
 };
 
-// export const configs: Record<string, TSESLint.Linter.Config>;
+export const rules = {
+  'no-trim-start': noTrimStart,
+};
